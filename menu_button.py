b580463da16a5263baf5aa84f20e53c8400b3636
Dzaki Gastia <dzaki.122140030@student.itera.ac.id>
2024-05-19 11:47:42 +0700

    Update menu_button.py
    tambahan menu dan penggantian beberapa file

--- menu_button.py
+++ menu_button.py
@@ -1,3 +1,5 @@
+import pygame
+
 class MenuButton():
     def __init__(self, image, pos, text_input, font, base_color, hovering_color, text_y_offset=0):
         self.image = image
@@ -28,3 +30,25 @@ class MenuButton():
             self.text = self.font.render(self.text_input, True, self.hovering_color)
         else:
             self.text = self.font.render(self.text_input, True, self.base_color)
+
+class MenuImageButton(MenuButton):
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color, size=None, border_color=None, border_width=0):
+        super().__init__(image, pos, text_input, font, base_color, hovering_color)
+        self.size = size
+        if self.size is not None:
+            self.image = pygame.transform.scale(self.image, self.size)
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+        self.border_color = border_color
+        self.border_width = border_width
+
+    def update(self, display):
+        # Gambar border
+        if self.border_color and self.border_width > 0:
+            border_rect = pygame.Rect(self.rect.topleft, self.rect.size)
+            pygame.draw.rect(display, self.border_color, border_rect, self.border_width)
+
+        # Gambar gambar tombol
+        display.blit(self.image, self.rect)
+        display.blit(self.text, self.text_rect)
+
